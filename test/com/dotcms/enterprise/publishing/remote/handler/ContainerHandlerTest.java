d4a0799fd9a5508598f09beb14b522376878b71a
Jorge Urdaneta <jorge.urdaneta@dotcms.com>
2013-09-05 16:02:18 -0430

    #2930 finished container handler test

--- test/com/dotcms/enterprise/publishing/remote/handler/ContainerHandlerTest.java
+++ test/com/dotcms/enterprise/publishing/remote/handler/ContainerHandlerTest.java
@@ -6,11 +6,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.dotcms.publisher.pusher.PushPublisherConfig.Operation;
 import com.dotcms.publisher.pusher.wrapper.ContainerWrapper;
 import com.dotcms.publishing.BundlerUtil;
+import com.dotcms.publishing.PublisherConfig;
 import com.dotmarketing.beans.ContainerStructure;
 import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
@@ -21,7 +23,6 @@ import com.dotmarketing.portlets.containers.model.ContainerVersionInfo;
 import com.dotmarketing.portlets.structure.model.Structure;
 import com.dotmarketing.util.UUIDGenerator;
 import com.liferay.portal.model.User;
-import com.liferay.util.FileUtil;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
@@ -48,7 +49,6 @@ public class ContainerHandlerTest {
         Container container=new Container();
         container.setInode(inode);
         container.setIdentifier(identId);
-        container.setCode("this is the code for container "+identId);
         container.setMaxContentlets(10);
         container.setModDate(Calendar.getInstance().getTime());
         container.setModUser(user.getUserId());
@@ -79,10 +79,35 @@ public class ContainerHandlerTest {
         cw.setCsList(csList);
         
         File bundle=BundlerUtil.getBundleRoot("test"+salt);
+        bundle.mkdirs();
+        
         File containerFile=new File(bundle,identId+".container.xml");
         XStream xstream=new XStream(new DomDriver());
         FileWriter fw=new FileWriter(containerFile);
         xstream.toXML(cw, fw);
         fw.close();
+        
+        PublisherConfig conf=new PublisherConfig();
+        conf.setId(salt);
+        
+        ContainerHandler handler=new ContainerHandler(conf);
+        handler.handle(bundle);
+        
+        Container created=APILocator.getContainerAPI().getWorkingContainerById(identId, user, false);
+        Assert.assertNotNull(created);
+        Assert.assertEquals(inode, created.getInode());
+        Assert.assertEquals(identId, created.getIdentifier());
+        Assert.assertEquals(false, created.isLive());
+        Assert.assertEquals("preloop111", created.getPreLoop());
+        Assert.assertEquals("postloop111", created.getPostLoop());
+        Assert.assertEquals(containerName, created.getTitle());
+        Assert.assertEquals(containerName, created.getFriendlyName());
+        Assert.assertEquals(10,created.getMaxContentlets());
+        
+        List<ContainerStructure> createdcr = APILocator.getContainerAPI().getContainerStructures(created);
+        Assert.assertEquals(1, createdcr.size());
+        Assert.assertEquals(st.getInode(),createdcr.get(0).getStructureId());
+        Assert.assertEquals(identId, createdcr.get(0).getContainerId());
+        Assert.assertEquals("$body", createdcr.get(0).getCode());
     }
 }
